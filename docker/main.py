6dbf42319f1b517c42d2b7a766567fdc0d26c9e8
alexZeakis <alzeakis@gmail.com>
2024-03-26 17:58:02 +0200

    Added metrics in main.

--- docker/main.py
+++ docker/main.py
@@ -6,19 +6,26 @@ import uuid
 import sys
 import importlib
 import traceback
+from sklearn.preprocessing import MultiLabelBinarizer
+from sklearn.metrics import precision_score, recall_score, f1_score
 
-def prep_df(input_file, col_text, col_separator, text_separator, minio):
+def prep_df(input_file, header, col_text, col_separator, text_separator, minio):
     """
     Prepare DataFrame from input file.
     """
+    
+    header = header if header != -1 else None
+    
     if input_file.startswith('s3://'):
         bucket, key = input_file.replace('s3://', '').split('/', 1)
         client = Minio(minio['endpoint_url'], access_key=minio['id'], secret_key=minio['key'])
-        df = pd.read_csv(client.get_object(bucket, key), header=None, sep=col_separator, on_bad_lines = 'warn')
+        df = pd.read_csv(client.get_object(bucket, key), header=header, sep=col_separator, on_bad_lines = 'warn')
     else:
-        df = pd.read_csv(input_file, header=None, sep=col_separator, on_bad_lines = 'warn')
+        df = pd.read_csv(input_file, header=header, sep=col_separator, on_bad_lines = 'warn')
+    df.columns = [str(c) for c in df.columns]
     
-    col_text = df.columns[col_text]
+    # col_text = df.columns[col_text]
+    df[f"{col_text}_original"] = df[col_text].copy()
     df[col_text] = df[col_text].str.split(text_separator)
     df = df.loc[~(df[col_text].isna())]
     df[col_text] = df[col_text].apply(lambda x: list(set(x)))
@@ -50,73 +57,102 @@ def run(j):
             col_id_left = j['parameters']['col_id_left']
             col_text_left = j['parameters']['col_text_left']
             col_separator_left = j['parameters']['col_separator_left']        
+            col_ground_left = j['parameters'].get('col_ground_left', None)
             text_separator_left = j['parameters']['text_separator_left']        
+            header_left = j['parameters']['header_left']
             input_file_right = inputs[1]
             col_id_right = j['parameters']['col_id_right']
             col_text_right = j['parameters']['col_text_right']        
             col_separator_right = j['parameters']['col_separator_right']                
             text_separator_right = j['parameters']['text_separator_right']            
+            header_right = j['parameters']['header_right']
             output_file = j['parameters']['output_file']
             
-            df_left = prep_df(input_file_left, col_text_left, col_separator_left, text_separator_left, minio)
-            df_right = prep_df(input_file_right, col_text_right, col_separator_right, text_separator_right, minio)
+            df_left = prep_df(input_file_left, header_left, col_text_left, col_separator_left, text_separator_left, minio)
+            df_right = prep_df(input_file_right, header_right, col_text_right, col_separator_right, text_separator_right, minio)
+            
+            left_attr = [f"{col_text_left}_original"]
+            if col_ground_left is not None:
+                left_attr.append(col_ground_left)
+            right_attr = [f"{col_text_right}_original"]
             
             if method == 'delta':
                 delta = j['parameters']['delta']     
                 posFilter = j['parameters']['posFilter']     
                 jointFilter = j['parameters']['jointFilter']   
                 pairs, log = module.tokenjoin_foreign(df_left, df_right, 
-                                                      str(col_id_left), str(col_id_right),
-                                                      str(col_text_left), str(col_text_right),
+                                                      col_id_left, col_id_right,
+                                                      col_text_left, col_text_right,
                                                       delta=delta, posFilter = posFilter,
                                                       jointFilter = jointFilter,
-                                                      keepLog=True)
+                                                      keepLog=True,
+                                                      left_attr=left_attr, right_attr=right_attr)
             elif method == 'topk':
                 k = j['parameters']['k']
                 delta_alg = j['parameters']['delta_alg']
                 pairs, log = module.tokenjoin_foreign(df_left, df_right, 
-                                                      str(col_id_left), str(col_id_right),
-                                                      str(col_text_left), str(col_text_right),
-                                                      k=k, delta_alg=delta_alg, keepLog=True)
+                                                      col_id_left, col_id_right,
+                                                      col_text_left, col_text_right,
+                                                      k=k, delta_alg=delta_alg, keepLog=True,
+                                                      left_attr=left_attr, right_attr=right_attr)
                 
             elif method == 'knn':
                 k = j['parameters']['k']
                 delta_alg = j['parameters']['delta_alg']
                 
                 pairs, log = module.tokenjoin_foreign(df_left, df_right, 
-                                                      str(col_id_left), str(col_id_right),
-                                                      str(col_text_left), str(col_text_right),
-                                                      k=k, delta_alg=delta_alg, keepLog=True)
+                                                      col_id_left, col_id_right,
+                                                      col_text_left, col_text_right,
+                                                      k=k, delta_alg=delta_alg, keepLog=True,
+                                                      left_attr=left_attr, right_attr=right_attr)
+                
+            if col_ground_left is not None:
+                mlb = MultiLabelBinarizer()
+                y_true_bin = mlb.fit_transform(pairs[f"l_{col_ground_left}"])
+                y_pred_bin = mlb.transform(pairs[f"r_{col_text_right}_original"])
+
+                log = {'total_time': log['total_time']}
+                for avg in ['micro', 'macro', 'weighted']:
+                    log[f'precision_{avg}'] = precision_score(y_true_bin, y_pred_bin, average=avg)
+                    log[f'recall_{avg}'] = recall_score(y_true_bin, y_pred_bin, average=avg)
+                    log[f'f1_{avg}'] = f1_score(y_true_bin, y_pred_bin, average=avg)
+                
         else:
             input_file = inputs[0]
             col_id = j['parameters']['col_id']
             col_text = j['parameters']['col_text']
             col_separator = j['parameters']['col_separator']
+            col_ground = j['parameters'].get('col_ground', None)
             text_separator = j['parameters']['text_separator']            
             output_file = j['parameters']['output_file']
             
             df = prep_df(input_file, col_text, col_separator, text_separator, minio)
+            attr = [f"{col_text}_original"]
+            if col_ground is not None:
+                attr.append(col_ground)
             
             if method == 'delta':
                 delta = j['parameters']['delta']     
                 posFilter = j['parameters']['posFilter']     
                 jointFilter = j['parameters']['jointFilter']     
-                pairs, log = module.tokenjoin_self(df, str(col_id), str(col_text),
+                pairs, log = module.tokenjoin_self(df, col_id, col_text,
                                                    delta=delta, posFilter = posFilter,
                                                    jointFilter = jointFilter,
-                                                   keepLog=True)
+                                                   keepLog=True, attr=attr)
             elif method == 'topk':
                 k = j['parameters']['k']
                 delta_alg = j['parameters']['delta_alg']
-                pairs, log = module.tokenjoin_self(df, str(col_id), str(col_text),
-                                                   k=k, delta_alg=delta_alg, keepLog=True)
+                pairs, log = module.tokenjoin_self(df, col_id, col_text,
+                                                   k=k, delta_alg=delta_alg, 
+                                                   keepLog=True, attr=attr)
                 
             elif method == 'knn':
                 k = j['parameters']['k']
                 delta_alg = j['parameters']['delta_alg']
                 
-                pairs, log = module.tokenjoin_self(df, str(col_id), str(col_text),
-                                                   k=k, delta_alg=delta_alg, keepLog=True)                
+                pairs, log = module.tokenjoin_self(df, col_id, col_text,
+                                                   k=k, delta_alg=delta_alg,
+                                                   keepLog=True, attr=attr)                
 
         pairs.to_csv(output_file)
 
@@ -129,6 +165,7 @@ def run(j):
         return {'message': 'TokenJoin project executed successfully!',
                 'output': [{'name': 'List of joined entities', 'path': object_path}], 'metrics': log, 'status': 200}
     except Exception as e:
+        print(traceback.format_exc())
         return {
             'message': 'An error occurred during data processing.',
             'error': traceback.format_exc(),
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="pytokenjoin",
-    version="0.1.7",
+    version="0.1.8",
     description="pyTokenJoin is a library containing efficient algorithms that solve the set similarity join problem with maximum weighted bipartite matching.",
     author="Alexandros Zeakis",
     author_email="azeakis@athenarc.gr",
@@ -12,7 +12,8 @@ setup(
         "editdistance==0.6.2",
         "networkx==3.1",
         "pandas==1.5.3",
-        "numpy==1.23.5"
+        "numpy==1.23.5",
+        "scikit-learn==1.3.2"
     ],
 )
 
